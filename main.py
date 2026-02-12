5b82d22a92d99f74d1b7c44328c07763db77b5ff
THSPSS <sunyeongbang98@gmail.com>
2026-02-12 20:11:51 +0900

    update with tk with title and set width and height of window

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import tkinter as tk
+
 #how to get a WPM
 #user typing two words than it gets a wpm
+# wpm is word per minute(60seconds)
+
+
+#make Gui
+if __name__ == "main" :
+
+    app = tk.Tk()
+    scrollbar = tk.Scrollbar(app, orient=tk.VERTICAL)
+
+    #set title of program and size
+    app.title("check speed of typing")
+    app.geometry("296*200")
